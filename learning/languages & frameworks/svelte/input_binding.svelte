00a3b8dd5332876ed6629d21eb313c3d5c006979
AquaInfinite <jeromeydawinner@gmail.com>
2024-09-15 00:03:42 -0400

    changed files around to match vscode

---
+++ learning/languages & frameworks/svelte/input_binding.svelte
@@ -0,0 +1,5 @@
+<script>
+    let user = ""
+</script>
+<input bind:value = {user}>
+<p>{user}</p>
---
+++ learning/languages & frameworks/svelte/on_click.svelte
@@ -0,0 +1,7 @@
+<script>
+    let count = 0
+    $: doubled = count * 2
+</script>
+
+<button on:click={() => {count += 1}}>Test</button>
+<p>{doubled}</p>
